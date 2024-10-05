ab1466ff739a91399f83152056230b289fa1f7ad
Darío Antonio Gutiérrez Álvarez <darioantoniogutierrezalvarez@MacBook-Air-de-Dario.local>
2024-10-04 20:42:42 -0600

    [ADD] fetch in frontend

--- lib/screens/catalogo.dart
+++ lib/screens/catalogo.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class CatalogoView extends StatelessWidget {
+class CatalogoView extends StatefulWidget {
+  @override
+  _CatalogoViewState createState() => _CatalogoViewState();
+}
+
+class _CatalogoViewState extends State<CatalogoView> {
+  List<dynamic> _instituciones = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchInstituciones();
+  }
+
+  Future<void> _fetchInstituciones() async {
+    final response = await http.get(Uri.parse('http://localhost:8080/api/instituciones'));
+    if (response.statusCode == 200) {
+      setState(() {
+        _instituciones = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load instituciones');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Catalogo de universidades'),
+        title: Text('Catálogo de universidades'),
       ),
-      body: Center(
-        child: Text('Uni'),
-      ),
+      body: _instituciones.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _instituciones.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_instituciones[index]['ssi_nombre']),
+                );
+              },
+            ),
     );
   }
 }
